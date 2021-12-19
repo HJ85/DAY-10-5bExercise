8dce7f69f0df390276d52daab3bb34cd49a1cc76
HJ85 <96196316+HJ85@users.noreply.github.com>
2021-12-19 19:14:43 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+"Hello world";
+$("p").css("color","purple")
+$("p").css("font-size","10px")
+$("p").text("COOL")
+
+//*    $(".North").css("background","purple")
+//* I do not know
+
+if ("p" > 100) {
+    console.log("whoah, that's a big number")
+} else {
+    console.log("just a regular number, please.")
+}
+
+if (".South" == "COOL") {
+    $(".South").text("Power od DOM")
+} else {
+    $(".West").text("Power of DOM")
+}
